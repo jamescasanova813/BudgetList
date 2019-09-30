ec81cc2b3aeb9b699d88fd416e5f2288b229ed43
jamescasanova813 <jamescasanova813@gmail.com>
2019-09-30 17:12:08 -0400

    testing

--- app/src/androidTest/java/com/example/budgetlist/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/budgetlist/ExampleInstrumentedTest.java
@@ -22,6 +22,5 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.example.budgetlist", appContext.getPackageName());
-
     }
 }
