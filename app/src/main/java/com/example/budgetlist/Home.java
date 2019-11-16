2a323c9ceaf7fe18415f940f0227bbe80a0abf32
AzulTiger <curtiss1@mail.usf.edu>
2019-11-16 13:01:22 -0500

    Set up the page fragments for the shopper's side of the app.

--- app/src/main/java/com/example/budgetlist/Home.java
+++ app/src/main/java/com/example/budgetlist/Home.java
@@ -2,14 +2,18 @@ package com.example.budgetlist;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
 
     @Override
@@ -21,12 +25,44 @@ public class Home extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+        if(savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new ShopFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_main_store);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch(menuItem.getItemId()){
+            case R.id.nav_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ProfileFragment()).commit();
+                break;
+            case R.id.nav_my_items:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new MyItemsFragment()).commit();
+                break;
+            case R.id.nav_main_store:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ShopFragment()).commit();
+                break;
+            case R.id.nav_settings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new SettingsFragment()).commit();
+                break;
+
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
